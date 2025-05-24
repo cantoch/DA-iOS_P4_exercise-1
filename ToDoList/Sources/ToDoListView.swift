2cc263956a797ca7d22fdbd7eb500c6190820457
cantoch <cantoch@icloud.com>
2025-05-25 00:42:25 +0200

    First commit

--- ToDoList/Sources/ToDoListView.swift
+++ ToDoList/Sources/ToDoListView.swift
@@ -11,7 +11,7 @@ struct ToDoListView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
+            VStack { 
                 // Filter selector
                 // TODO: - Add a filter selector which will call the viewModel for updating the displayed data
                 // List of tasks
